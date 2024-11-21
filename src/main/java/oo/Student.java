dbae489e20a9e4b075ba98ed3ef126363020e7d5
Jackson Tam <Jackson.Tam@OOCL.com>
2024-11-21 14:00:05 +0800

    [OO-step-by-step] feat: create student class

--- src/main/java/oo/Student.java
+++ src/main/java/oo/Student.java
@@ -1,4 +1,19 @@
 package oo;
 
-public class Student {
+public class Student extends Person {
+
+    public Student(int id, String name, int age) {
+        super(id,name,age);
+    }
+
+
+    @Override
+    public String introduce () {
+        return "My name is " + this.getName() + ". I am " + this.getAge() + " years old."+ " I am a student.";
+    }
+
+
+
+
+
 }
