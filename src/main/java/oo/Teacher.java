e319108dfce5f194067c81a8a9ebc629a656ff76
Jackson Tam <Jackson.Tam@OOCL.com>
2024-11-21 14:03:29 +0800

    [OO-step-by-step] feat: create Teacher class

--- src/main/java/oo/Teacher.java
+++ src/main/java/oo/Teacher.java
@@ -1,4 +1,14 @@
 package oo;
 
-public class Teacher {
+public class Teacher extends Person {
+
+    public Teacher(int id, String name, int age) {
+        super(id,name,age);
+    }
+
+
+    @Override
+    public String introduce () {
+        return "My name is " + this.getName() + ". I am " + this.getAge() + " years old."+ " I am a teacher.";
+    }
 }
