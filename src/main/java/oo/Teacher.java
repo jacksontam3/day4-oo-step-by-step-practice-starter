c0ce1f5de8e3869832c59bad86b82473fd4d9244
Jackson Tam <Jackson.Tam@OOCL.com>
2024-11-21 14:42:48 +0800

    [OO-step-by-step] feat: adding teacher assignTo and belongsTo

--- src/main/java/oo/Teacher.java
+++ src/main/java/oo/Teacher.java
@@ -3,6 +3,7 @@ package oo;
 public class Teacher extends Person {
 
     public static final String OCCUPATION = "teacher";
+    private Klass klass;
 
     public Teacher(int id, String name, int age) {
         super(id,name,age);
@@ -11,5 +12,14 @@ public class Teacher extends Person {
     @Override
     public String introduce () {
         return String.format("My name is %s. I am %d years old. I am a %s.",getName(), getAge(), OCCUPATION);
+    }
+
+    public void assignTo(Klass klass) {
+        this.klass = klass;
+    }
+
+    public boolean belongsTo(Klass klass) {
+        if (this.klass == null)return false;
+        return this.klass.equals(klass);
     }
 }
