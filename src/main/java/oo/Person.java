0075be9cff2c94756e00932480d22cc89c605e9c
Jackson Tam <Jackson.Tam@OOCL.com>
2024-11-21 13:59:51 +0800

    [OO-step-by-step] feat: create getter and setter for Person.java

--- src/main/java/oo/Person.java
+++ src/main/java/oo/Person.java
@@ -30,4 +30,28 @@ public class Person {
     public int hashCode() {
         return Objects.hashCode(id);
     }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
