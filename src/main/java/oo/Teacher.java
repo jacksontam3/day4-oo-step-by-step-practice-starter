5901673c873c2efa1c4e1a70a7cd106fdb0dde35
Jackson Tam <Jackson.Tam@OOCL.com>
2024-11-21 14:22:36 +0800

    [OO-step-by-step] feat: modify student introduce message

--- src/main/java/oo/Teacher.java
+++ src/main/java/oo/Teacher.java
@@ -2,13 +2,14 @@ package oo;
 
 public class Teacher extends Person {
 
+    public static final String OCCUPATION = "teacher";
+
     public Teacher(int id, String name, int age) {
         super(id,name,age);
     }
 
-
     @Override
     public String introduce () {
-        return "My name is " + this.getName() + ". I am " + this.getAge() + " years old."+ " I am a teacher.";
+        return String.format("My name is %s. I am %d years old. I am a %s.",getName(), getAge(), OCCUPATION);
     }
 }
