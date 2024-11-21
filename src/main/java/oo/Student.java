ab0834d311a8ba6b0d6b5dca253bd981492b8b07
Jackson Tam <Jackson.Tam@OOCL.com>
2024-11-21 14:33:13 +0800

    [OO-step-by-step] feat: adding isIN and join method for klass for student

--- src/main/java/oo/Student.java
+++ src/main/java/oo/Student.java
@@ -4,6 +4,8 @@ public class Student extends Person {
 
     public static final String OCCUPATION = "student";
 
+    private Klass klass;
+
     public Student(int id, String name, int age) {
         super(id,name,age);
     }
@@ -12,6 +14,19 @@ public class Student extends Person {
     @Override
     public String introduce () {
         return String.format("My name is %s. I am %d years old. I am a %s.",getName(), getAge(), OCCUPATION);
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public void join(Klass klass) {
+        this.klass = klass;
+    }
+
+    public boolean isIn (Klass klass){
+        if(klass != null) return klass == this.klass;
+        return false;
     }
 
 
