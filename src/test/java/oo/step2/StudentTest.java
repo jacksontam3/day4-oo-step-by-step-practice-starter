add2f61e51ed556f176d3d023c2e16781a645020
Jackson Tam <Jackson.Tam@OOCL.com>
2024-11-21 14:01:15 +0800

    [OO-step-by-step] test: should_return_true_when_equals_given_two_students_with_same_id

--- src/test/java/oo/step2/StudentTest.java
+++ src/test/java/oo/step2/StudentTest.java
@@ -22,14 +22,14 @@ public class StudentTest {
 
         assertThat(introduce).isEqualTo("My name is Jim. I am 19 years old. I am a student.");
     }
-//
-//    @Test
-//    public void should_return_true_when_equals_given_two_students_with_same_id() {
-//        Student student1 = new Student(1, "Tom", 18);
-//        Student student2 = new Student(1, "Tommy", 19);
-//
-//        assertThat(student1).isEqualTo(student2);
-//    }
+
+    @Test
+    public void should_return_true_when_equals_given_two_students_with_same_id() {
+        Student student1 = new Student(1, "Tom", 18);
+        Student student2 = new Student(1, "Tommy", 19);
+
+        assertThat(student1).isEqualTo(student2);
+    }
 //
 //    @Test
 //    public void should_return_false_when_equals_given_two_students_with_different_id() {
